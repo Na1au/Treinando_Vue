57ce395b8fa1b472ae2e1d9e1521bcc0fa84aaba
Na1au <analaura-osilva@hotmail.com>
2022-08-08 20:41:52 -0300

    modified files

---
+++ projeto_inicial/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <NavBar :logo="logo_src" :alt="app_name"/>
+  <router-view/>
+  <MyFooter/>
+</template>
+
+<script>
+  import NavBar from './components/NavBar.vue';
+  import MyFooter from './components/MyFooter.vue';
+
+  export default {
+    components:  {
+      NavBar,
+      MyFooter
+    },
+    data() {
+      return {
+        logo_src: "/img/logo.png",
+        app_name: "Make your burguer"
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
